c1a0c6bcad4d7064e7d1469eb6d0c7885b759991
nymajona <nymajona@arcada.fi>
2022-10-05 10:53:11 +0000

    com

--- app.py
+++ app.py
@@ -8,6 +8,17 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+
+    if request.method == 'GET':
+        return {
+            'method': request.method,
+            'msg': 'Github webhook deployment works!',
+            'env': os.environ.get('ENV_VAR', 'Cannot find variable ENV_VAR')
+        }
+    if request.method == 'POST':
+        return{
+            'msg': 'You posted something',
+        }
     ret = { 
         'msg': 'Flask works on Rahti!', 
         'env': os.environ.get('ENV_VAR', 'Cannot find variable ENV_VAR') 
